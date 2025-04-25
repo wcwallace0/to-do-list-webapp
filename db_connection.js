09645b8b2c0c70152d59c6ed42da499bacecf2f1
Will Wallace <wwallac1@trinity.edu>
2025-04-25 18:47:54 -0500

    Setup PostgreSQL database connection

---
+++ db_connection.js
@@ -0,0 +1,17 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+const connection = new Pool({
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_NAME
+});
+
+// Prints message to server console when database is queried
+connection.on('connect', (client) => {
+    console.log("PostgreSQL accessed.");
+})
+
+exports.db_connection = connection;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require('dotenv').config();
+const db = require("./db_connection.js").db_connection;
 
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: false}))
@@ -9,9 +10,21 @@ app.set("views", "views");
 app.set("view engine", "pug");
 
 app.get("/", function(req, res) {
+    // testDatabaseConnection();
     res.render("home");
 });
 
 app.listen(3018, function() {
     console.log("Listening on port 3018...");
 });
+
+function testDatabaseConnection() {
+    let sql = "SELECT * FROM Usr";
+    db.query(sql)
+        .then(result => {
+            console.log(result);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
